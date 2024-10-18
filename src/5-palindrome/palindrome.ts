b1a9825f6645730d629b1888cfad42f2d0af85db
Espanhola <107200714+AdelitaLegal@users.noreply.github.com>
2024-10-17 23:22:31 -0300

    Update palindrome.ts

--- src/5-palindrome/palindrome.ts
+++ src/5-palindrome/palindrome.ts
@@ -1,3 +1,10 @@
-export const sort = (arr: string[]): string[] => {
-  return arr.sort();
+export const isPalindrome = (str: string): boolean => {
+  const len = str.length;
+
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false; // Retorna falso se os caracteres não forem iguais
+    }
+  }
+  return true; // Retorna verdadeiro se for um palíndromo
 };
